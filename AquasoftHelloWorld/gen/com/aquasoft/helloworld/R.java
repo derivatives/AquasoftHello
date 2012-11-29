446ae6170d511bfad2414e3e19024067adc99be7
AquaSoft <AquaSoft@aquasoft-pc>
2012-11-29 15:09:24 +0700

    commit laout add textview

--- AquasoftHelloWorld/gen/com/aquasoft/helloworld/R.java
+++ AquasoftHelloWorld/gen/com/aquasoft/helloworld/R.java
@@ -31,6 +31,7 @@ public final class R {
         public static final int Hello_Opor=0x7f040004;
         public static final int Hello_Tong=0x7f040005;
         public static final int Hello_nu=0x7f040003;
+        public static final int Warayut=0x7f040006;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
