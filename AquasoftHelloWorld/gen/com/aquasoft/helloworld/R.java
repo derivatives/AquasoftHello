765cd8ed22b138cf1f6ab175d0023ab33ea1ef0d
tong__000 <tong__000@Anupan>
2012-11-29 11:27:54 +0700

    Add Tong

--- AquasoftHelloWorld/gen/com/aquasoft/helloworld/R.java
+++ AquasoftHelloWorld/gen/com/aquasoft/helloworld/R.java
@@ -14,10 +14,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070003;
+        public static final int menu_settings=0x7f070004;
         public static final int textView1=0x7f070001;
         public static final int textView2=0x7f070000;
         public static final int textView_opor=0x7f070002;
+        public static final int textView_tong=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -27,6 +28,7 @@ public final class R {
     }
     public static final class string {
         public static final int Hello_Opor=0x7f040004;
+        public static final int Hello_Tong=0x7f040005;
         public static final int Hello_nu=0x7f040003;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
