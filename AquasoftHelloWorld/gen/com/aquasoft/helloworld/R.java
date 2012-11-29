b80d7705e98eaec23a667fb93d7d30c991508a94
AquaSoft <AquaSoft@aquasoft-pc>
2012-11-29 15:01:39 +0700

    commit add aqua

--- AquasoftHelloWorld/gen/com/aquasoft/helloworld/R.java
+++ AquasoftHelloWorld/gen/com/aquasoft/helloworld/R.java
@@ -14,9 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070005;
+        public static final int menu_settings=0x7f070006;
         public static final int textView1=0x7f070001;
         public static final int textView2=0x7f070000;
+        public static final int textView_aqua=0x7f070005;
         public static final int textView_opor=0x7f070002;
         public static final int textView_tong=0x7f070003;
         public static final int textView_warayut=0x7f070004;
@@ -30,6 +31,7 @@ public final class R {
     public static final class string {
         public static final int Hello_Opor=0x7f040004;
         public static final int Hello_Tong=0x7f040005;
+        public static final int Hello_aqua=0x7f040007;
         public static final int Hello_nu=0x7f040003;
         public static final int Hello_warayut=0x7f040006;
         public static final int app_name=0x7f040000;
